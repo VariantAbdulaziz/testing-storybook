28aa3442d8bffe43f48647fad15143b2c34633e7
Abdulaziz Ali <variant.abdulaziz@gmail.com>
2024-10-08 22:52:40 +0300

    check visual change

--- components/button.vue
+++ components/button.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="px-4 py-2 bg-blue-400 text-white rounded hover:bg-blue-600">
+  <button class="px-4 py-2 bg-blue-200 text-white rounded hover:bg-blue-700">
     {{ label }}
   </button>
 </template>
